65c46a781188dca3c946462f5ec4238c6b62c680
GuGU <wnduddl3409@naver.com>
2019-12-06 13:20:58 +0900

    ADD Suggest Movies

--- graphql/db.js
+++ graphql/db.js
@@ -1,16 +1,19 @@
 import axios from "axios";
 const BASE_URL = "https://yts.lt/api/v2/";
-const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_LISTS_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
+const GET_STATUS_URL = MOVIE_LISTS_URL;
 
 export const getStatus = async() => {
   let response;
   try {
-    response = await axios.get(LIST_MOVIES_URL);
+    response = await axios.get(GET_STATUS_URL);
   } catch(err) {
     console.error(err);
     return err;
   }
-  return response.status;
+  return response.data.status;
 };
 
 export const getMovies = async(limit, rating) => {
@@ -18,11 +21,42 @@ export const getMovies = async(limit, rating) => {
     data: {
       data: { movies }
     }
-  } = await axios(LIST_MOVIES_URL, {
+  } = await axios(MOVIE_LISTS_URL, {
     params: {
       limit,
       rating
     }
   });
+  console.log(movies);
   return movies;
 };
+
+export const getMovieById = async(id) => {
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id : id
+    }
+  });
+  console.log(movie);
+  return movie;
+};
+
+export const suggestMovieById = async(id) => {
+  const {
+    data: {
+      data: { suggest_movies }
+    }
+  } = await axios.get(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id : id
+    }
+  });
+  console.log(suggest_movies);
+  return suggest_movies;
+};
+
+
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,11 +1,12 @@
-import { getStatus, getMovies } from './db';
+import { getStatus, getMovies, getMovieById, suggestMovieById } from './db';
 
 const resolvers = {
-    Query: {
-        status: () => getStatus(),
-        movies: (_, {limit, rating}) => getMovies(limit, rating)
-    }
+  Query: {
+    status: () => getStatus(),
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovieById(id),
+    suggest: (_, { id }) => suggestMovieById(id)
+  }
 };
 
 export default resolvers;
- 
